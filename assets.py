0e569d6633c45916e394ef7f7d2b8e3c11ea8196
SAJacob7 <sophiajacob@ku.edu>
2025-08-24 21:35:38 -0500

    add logic for random bombs on grid

--- assets.py
+++ assets.py
@@ -1,6 +1,7 @@
 import pygame
 from settings import *
-
+import random
+# Types: ~ = Unkown, B = Bomb, E = Empty
 class Ceil:
     def __init__(self, x, y, image, type, revealed=False, flagged=False):
         self.x = x * TILESIZE
@@ -12,3 +13,14 @@ class Ceil:
 
     def __type__(self):
         return self.type
+
+class Grid:
+    def generate_bombs(self, total_bombs):
+        for i in range(total_bombs):
+            while True:
+                bomb_x_coord = random.randint(0, ROWS-1)
+                bomb_y_coord = random.randint(0, COLUMNS - 1)
+                if self.grid_list[bomb_x_coord][bomb_y_coord].type == "~":
+                    self.grid_list[bomb_x_coord][bomb_y_coord].type = "B"
+                    self.grid_list[bomb_x_coord][bomb_y_coord].image = bomb_tile
+                    break
