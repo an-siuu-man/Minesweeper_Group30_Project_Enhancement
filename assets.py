8a4236f675bff2e93deb86eb15011a841ac73a31
SAJacob7 <sophiajacob@ku.edu>
2025-08-24 23:22:42 -0500

    fixed code so that it generates the grid and random bombs with ui

--- assets.py
+++ assets.py
@@ -4,26 +4,36 @@ import random
 # Types: ~ = Unkown, B = Bomb, E = Empty, test
 class Cell:
     def __init__(self, x, y, image, type, revealed=False, flagged=False):
-        self.x = x * TILESIZE
-        self.y = y * TILESIZE
+        self.x = x * CELLSIZE
+        self.y = y * CELLSIZE
         self.image = image
         self.type = type
         self.revealed = revealed
         self.flagged = flagged
 
-    def __type__(self):
+    def draw(self, grid_surface):
+        grid_surface.blit(self.image, (self.x, self.y))
+
+
+    def __repr__(self):
         return self.type
 
 class Grid:
     def __init__(self):
        self.grid_surface = pygame.Surface((WIDTH, HEIGHT))
-       self.grid_list = [[Cell(col, row, empty_tile, ".") for row in range(ROWS)] for col in range(COLUMNS)]
+       self.grid_list = [[Cell(col, row, empty_cell, "~") for row in range(ROWS)] for col in range(COLUMNS)]
        self.generate_bombs()
-  
+
     def display_board(self):
        for row in self.grid_list:
            print(row)
-    
+
+    def draw(self, screen):
+        for row in self.grid_list:
+            for cell in row:
+                cell.draw(self.grid_surface)
+        screen.blit(self.grid_surface, (0, 0))
+
     def generate_bombs(self):
         for i in range(BOMB_AMT):
             while True:
@@ -31,5 +41,5 @@ class Grid:
                 bomb_y_coord = random.randint(0, COLUMNS - 1)
                 if self.grid_list[bomb_x_coord][bomb_y_coord].type == "~":
                     self.grid_list[bomb_x_coord][bomb_y_coord].type = "B"
-                    self.grid_list[bomb_x_coord][bomb_y_coord].image = bomb_tile
+                    self.grid_list[bomb_x_coord][bomb_y_coord].image = bomb_cell
                     break
--- settings.py
+++ settings.py
@@ -1,10 +1,13 @@
+import pygame
+import os
+
 #Colors 
 BLACK = (0,0,0)
 WHITE = (255,255,255)
 RED = (255,0,0)
 LIGHTGREEN = (144)
 GREEN = (0,255,0)
-DARKGREEN = (0,255,0)
+DARKGREEN = (0,128,0)
 BLUE = (0,0,255)
 DARKBLUE = (0,0,139)
 LIGHTBLUE = (173,216,230)
@@ -14,23 +17,23 @@ LIGHTGREY = (100,100,100)
 BGCOLOR = DARKGREEN
 
 #Game settings
-TILESIZE = 32
+CELLSIZE = 50
 ROWS = 10
 COLUMNS = 10
 BOMB_AMT = 10 #Need to update so user-sepecifed from 10 to 20 
-WIDTH = TILESIZE * ROWS
-HEIGHT = TILESIZE * COLUMNS
+WIDTH = CELLSIZE * ROWS
+HEIGHT = CELLSIZE * COLUMNS
 FPS = 60
 TITLE = "Minesweeper"
 
 #Import assets 
-tile_num = []
+cell_num = []
 for i in range(1,9): 
-    tile_num.append(pygame.transform.scale(pygame.image.load(os.path.join("assets", f"Cell{i}.png")), (TILESIZE, TILESIZE)))
+    cell_num.append(pygame.transform.scale(pygame.image.load(os.path.join(f"Cell{i}.png")), (CELLSIZE, CELLSIZE)))
 
-empty_tile = pygame.transform.scale(pygame.image.load(os.path.join("assets", f"CellEmpty.png")), (TILESIZE, TILESIZE)) #Update asset names accordingly 
-exploded_tile = pygame.transform.scale(pygame.image.load(os.path.join("assets", f"CellExploded.png")), (TILESIZE, TILESIZE))
-flag_tile = pygame.transform.scale(pygame.image.load(os.path.join("assets", f"CellFlag.png")), (TILESIZE, TILESIZE))
-bomb_tile = pygame.transform.scale(pygame.image.load(os.path.join("assets", f"CellBomb.png")), (TILESIZE, TILESIZE))
-unknown_tile = pygame.transform.scale(pygame.image.load(os.path.join("assets", f"CellUnknown.png")), (TILESIZE, TILESIZE))
-nobomb_tile = pygame.transform.scale(pygame.image.load(os.path.join("assets", f"CellNoBomb.png")), (TILESIZE, TILESIZE))
+empty_cell = pygame.transform.scale(pygame.image.load(os.path.join(f"CellEmpty.png")), (CELLSIZE, CELLSIZE)) #Update asset names accordingly 
+exploded_cell = pygame.transform.scale(pygame.image.load(os.path.join(f"CellExploded.png")), (CELLSIZE, CELLSIZE))
+flag_cell = pygame.transform.scale(pygame.image.load(os.path.join("CellFlag.png")), (CELLSIZE, CELLSIZE))
+bomb_cell = pygame.transform.scale(pygame.image.load(os.path.join(f"CellBomb.png")), (CELLSIZE, CELLSIZE))
+unknown_cell = pygame.transform.scale(pygame.image.load(os.path.join(f"CellUnknown.png")), (CELLSIZE, CELLSIZE))
+nobomb_cell = pygame.transform.scale(pygame.image.load(os.path.join(f"CellNoBomb.png")), (CELLSIZE, CELLSIZE))
