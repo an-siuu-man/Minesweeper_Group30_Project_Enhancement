fc69386bfc8c51371a8551cad51deaf8ee826b39
KusumaMurthy109 <kusumamurthy109@gmail.com>
2025-08-24 21:12:41 -0500

    created empty green board

--- game.py
+++ game.py
@@ -1,8 +1,12 @@
 import pygame
+
 DARKGREEN = (0,255,0)
+
 class Game:
-    def __init__ (self):
-        self.layout = pygame.display.set_mode((10*40, 10*40))
+    def __init__(self):
+        pygame.init()
+
+        self.layout = pygame.display.set_mode((10*50, 10*50))
 
         pygame.display.set_caption("Minesweeper")
 
@@ -16,6 +20,17 @@ class Game:
         gridStructure = [[0 for i in range(10)] for i in range(10)]
         return gridStructure
     
+    def playGame(self):
+        while (self.isGameActive == True):
+            self.timer.tick(60)
+            for action in pygame.event.get():
+                if action.type == pygame.QUIT:
+                    self.isGameActive = False
+            
+            self.layout.fill(DARKGREEN)
+            pygame.display.update()
+        
+        pygame.quit()
 
 
 
